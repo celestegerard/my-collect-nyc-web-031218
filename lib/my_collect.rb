76821dcfd8f8459628c89acc95914cc53f6fe171
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-02 01:36:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(students)
+  newArray = []
+breakApart = students.join(" ")
 
+newArray << breakApart.split
+  i = 0
 
+while i < newArray.length  
+  yield array[0]
+  array.collect do |names|
+  
+  i = i + 1
+end
+array
+end
+
+newArray.collect do |names|
+  delete_names.even?
+  
+  yeilding(array) .collect
+  array.collect do |names|
+  delete_names.even?
